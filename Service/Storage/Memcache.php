ae0d3a8b812261af1f2dfc1e8825a9b2c5d0fe72
Joshua Thijssen <jthijssen@noxlogic.nl>
2014-06-01 16:42:16 +0200

    Removed todos in memcache storage

--- Service/Storage/Memcache.php
+++ Service/Storage/Memcache.php
@@ -12,25 +12,21 @@ class Memcache implements StorageInterface
 
     public function getRateInfo($key)
     {
-        // TODO: Implement getInfo() method.
         // @codeCoverageIgnoreStart
     }
 
     public function limitRate($key)
     {
-        // TODO: Implement limit() method.
         // @codeCoverageIgnoreStart
     }
 
     public function createRate($key, $limit, $period)
     {
-        // TODO: Implement createRate() method.
         // @codeCoverageIgnoreStart
     }
 
     public function resetRate($key)
     {
-        // TODO: Implement resetRateLimit() method.
         // @codeCoverageIgnoreStart
     }
 }
