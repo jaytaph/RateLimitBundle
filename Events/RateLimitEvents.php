a00ca57035f40a38c818c57ca65d99f3bb2ca588
Alexander Ivanov <sane4ek-2@yandex.ru>
2018-06-28 08:35:09 +0300

    Add a new event is dispatched after a block happened
    We need this event for making different things when a block happens.

--- Events/RateLimitEvents.php
+++ Events/RateLimitEvents.php
@@ -5,4 +5,9 @@ namespace Noxlogic\RateLimitBundle\Events;
 final class RateLimitEvents
 {
         const GENERATE_KEY = 'ratelimit.generate.key';
+
+    /**
+     * This event is dispatched after a block happened
+     */
+    const AFTER_BLOCK = 'ratelimit.block.after';
 }
