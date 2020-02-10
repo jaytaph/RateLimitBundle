1ba00e315e6583f55b3f9c2d62b83b192897a974
Kai <k.dederichs@spicymedia.io>
2020-02-10 16:55:04 +0100

    Fix type problems

--- Events/ProxyFilterResponseEvent.php
+++ Events/ProxyFilterResponseEvent.php
@@ -5,7 +5,7 @@ namespace Noxlogic\RateLimitBundle\Events;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent as LegacyEvent;
 
-if (!class_exists('Symfony\\Component\HttpKernel\\Event\\ResponseEvent')) {
+if (!class_exists('Symfony\\Component\\HttpKernel\\Event\\ResponseEvent')) {
     /**
      * Symfony 3.4
      */
