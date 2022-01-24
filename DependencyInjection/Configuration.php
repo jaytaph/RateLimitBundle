c2f0648fba7a409a321c632f5d9bca9be5d0b2f1
Alexis Urien <alexis@smiile.com>
2022-01-24 15:41:05 +0100

    Remove a deprecation on symfony 5.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('noxlogic_rate_limit');
         // Keep compatibility with symfony/config < 4.2
